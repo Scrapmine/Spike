79667d87c167ea4e5560cf62787559387bf1af16
Scrapmine <sondre.djupvikvag@gmail.com>
2022-05-15 16:24:40 +0100

    Did stuff

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
+  mainWindow.setTitle('Electron')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -24,7 +25,7 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
-
+  window.setTitle("Spike")
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
